8a0eeb8fd7e8af2a62dce28b9fd69781ad23a7f0
favour mogaji <favourmogaji3@gmail.com>
2026-06-07 20:55:23 -0500

    about section done

--- devportfolio/src/components/About.jsx
+++ devportfolio/src/components/About.jsx
@@ -1,4 +1,8 @@
 import me from '../assets/IMG_3448.jpg'
+import { FaAward } from "react-icons/fa6";
+import { FaUsersLine } from "react-icons/fa6";
+import { LuFolderCheck } from "react-icons/lu";
+
 function About() {
   return (
     <section id="about">
@@ -16,11 +20,30 @@ function About() {
         <div className="about_content">
           <div className='about_cards'>
             <article className='about_card'>
+              <FaAward  className='about_icon' />
+              <h5> Experience</h5>
+              <small>1+</small>
+
+            </article>
+            <article className='about_card'>
+             <FaUsersLine  className='about_icon'/>
+              <h5> Clients</h5>
+              <small>10+ worldwide</small>
+
+            </article>
+            <article className='about_card'>
+             <LuFolderCheck className='about_icon'/>
+              <h5> Project</h5>
+              <small>20+ Completed</small>
 
             </article>
 
           </div>
 
+          <p>Electronics and Computer Engineering student skilled in web and mobile development with React, Next.js,
+              Node.js, and SQL. Passionate about building real-world applications, digital content creation, and growing
+              into a professional Software Engineer.</p>
+              <a href="#contact" className='btn btn-primary'> Lets Talk</a>
         </div>
 
 
