025ffd3094a73ca75b86cce1fa72221660ad7e64
favour mogaji <favourmogaji3@gmail.com>
2026-06-08 05:56:13 -0500

    working on the experience section

--- devportfolio/src/components/Experience.jsx
+++ devportfolio/src/components/Experience.jsx
@@ -1,44 +1,74 @@
 import { IoMdCheckmarkCircle } from "react-icons/io";
 function Experience(){
   return(
-    <>
+  
     <section id="experience"> 
       <h5>Skills i have</h5>
       <h2>My Experience</h2>
 
       <div className = "container experience_container">
+
         <div className="experience_frontend">
+
           <h3>Frontend Development</h3>
+
           <div className="experience_content">
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+              <IoMdCheckmarkCircle  className="experiencedetails_icon"/>
+              <div>
             <h4>HTML</h4>
             <small className="text-light">Experienced</small>
+            </div>
              </article>
+
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+              <IoMdCheckmarkCircle  className="experiencedetails_icon"/>
+              <div>
             <h4>CSS</h4>
             <small className="text-light">Experienced</small>
+              </div>
              </article>
+
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+              <IoMdCheckmarkCircle  className="experiencedetails_icon" />
+              <div>
             <h4>BOOTSTRAP</h4>
             <small className="text-light">Experienced</small>
+              </div>
              </article>
+
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+              <IoMdCheckmarkCircle  className="experiencedetails_icon" />
+
+              <div>
             <h4>TAILWINDCSS</h4>
             <small className="text-light">Experienced</small>
+
+              </div>
              </article>
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+              <IoMdCheckmarkCircle  className="experiencedetails_icon" />
+              <div>
             <h4>JAVASCRIPT</h4>
             <small className="text-light">Experienced</small>
+
+              </div>
              </article>
+
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+              <IoMdCheckmarkCircle  className="experiencedetails_icon"/>
+
+              <div>
             <h4>REACT</h4>
             <small className="text-light">Experienced</small>
+
+              </div>
              </article>
 
           </div>
@@ -47,32 +77,65 @@ function Experience(){
         </div>
         {/* bACKEND */}
          <div className="experience_backend">
+
           <h3>Backend Development</h3>
           <div className="experience_content">
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+
+              <IoMdCheckmarkCircle  className="experiencedetails_icon"/>
+
+              <div>
             <h4>NODE JS</h4>
             <small className="text-light">Experienced</small>
+
+              </div>
              </article>
+
+
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+              <IoMdCheckmarkCircle  className="experiencedetails_icon"/>
+              <div>
+
             <h4>MANGODB</h4>
             <small className="text-light">Experienced</small>
+
+              </div>
+
              </article>
+
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+              <IoMdCheckmarkCircle  className="experiencedetails_icon"/>
+              <div>
+
             <h4>PHP</h4>
             <small className="text-light">Experienced</small>
+
+              </div>
              </article>
+
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+              <IoMdCheckmarkCircle  className="experiencedetails_icon"/>
+              <div>
+
             <h4>MYSQL</h4>
             <small className="text-light">Experienced</small>
+
+              </div>
              </article>
+
+
             <article className="experience_details">
-              <IoMdCheckmarkCircle />
+              <IoMdCheckmarkCircle  className="experiencedetails_icon"/>
+              <div>
+
             <h4>PYTHON</h4>
             <small className="text-light">Experienced</small>
+
+              </div>
              </article>
            
 
@@ -85,7 +148,7 @@ function Experience(){
 
 
     </section>
-    </>
+  
   )
 }
 
