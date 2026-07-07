ef490fd64ad24c5b98a43fa9c74324f4d147aa39
favour mogaji <favourmogaji3@gmail.com>
2026-07-07 08:16:10 -0500

    done

--- devportfolio/src/components/Navbar.jsx
+++ devportfolio/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaRegUser } from "react-icons/fa";
 import { FaAddressBook } from "react-icons/fa";
 import { RiServiceLine } from "react-icons/ri";
 import { BiSolidContact } from "react-icons/bi";
-import { SiImessage } from "react-icons/si";
+
 import { GrProjects } from "react-icons/gr";
 import { useState } from "react";
 function Navbar() {
@@ -16,7 +16,7 @@ function Navbar() {
       <a href="#services"onClick={ () => setactiveNav('#services')} className={activeNav === "#services"? 'active' : ""}  >< RiServiceLine /></a>
       <a href="#projects"onClick={ () => setactiveNav('#projects')} className={activeNav === "#projects"? 'active' : ""}  ><GrProjects /></a>
       <a href="#contact"onClick={ () => setactiveNav('#contact')} className={activeNav === "#contact"? 'active' : ""}  >< BiSolidContact /></a>
-      <a href="#message"onClick={ () => setactiveNav('#message')} className={activeNav === "#message"? 'active' : ""}  ><  SiImessage /></a>
+    
          </nav>
   )
 }
